8194b29b1042f8c212651a4becfca104f4bebc96
HangmansMoose <trivial86@protonmail.com>
2022-03-07 14:22:34 +1100

    added to existing code base

---
+++ Board.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Board {
+
+public:
+	void drawBoard(const Board*);
+	bool newPosition(int, int);
+
+
+private:
+
+};
---
+++ TicTacToe.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Board.h"
+#include "Player.h"
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Waddup Derds\n";
+	return 0;
+}
